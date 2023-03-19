d176170c70e5aedc264caf59d3271eba814c6b38
Ganesh Narasimhadevara <ganeshmuralidhar@gmail.com>
2023-03-19 21:21:22 +0530

    Added Alert conditions to Service Levels

--- terraform/allResources.tf
+++ terraform/allResources.tf
@@ -208,9 +208,6 @@ resource "newrelic_nrql_alert_condition" "ms-demo-sl-compliance-condition" {
   aggregation_window             = 60
   aggregation_method             = "event_flow"
   aggregation_delay              = 30
-  expiration_duration            = 120
-  open_violation_on_expiration   = true
-  close_violations_on_expiration = true
   slide_by                       = 30
 
   nrql {
